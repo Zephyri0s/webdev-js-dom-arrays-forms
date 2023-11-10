f5ce3d66599881a961f912eb4e15fdead8236bf3
Molnar <c3035956@hallam.shu.ac.uk>
2023-11-10 10:42:30 +0000

    yeee

--- JS/Test.js
+++ JS/Test.js
@@ -1,3 +1,5 @@
 (function(){
     console.info(document.getElementById('myHeading').innerHTML)
+    document.querySelector('nav ul li a').setAttribute('href', 'https://mytimetable.shu.ac.uk/schedule');
+    document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 })();
